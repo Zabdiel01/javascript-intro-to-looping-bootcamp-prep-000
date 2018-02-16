c55b6a02582f0564e36b5152a4a749b7d75d9b00
Zabdiel Ramos <zabdiel.ramos01@gmail.com>
2018-02-16 20:23:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,7 +18,11 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+  }
   do {
-    
-  } while ();
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array
 }
